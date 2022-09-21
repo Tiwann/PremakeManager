41a1c4e1148ea71337961698365a40792c1596f0
Tiwann <tiwannbeats@gmail.com>
2022-09-21 21:05:49 +0200

    Get asset paths easily

---
+++ PremakeManager/Include/Assets.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <filesystem>
+
+class Assets
+{
+public:
+    static std::filesystem::path GetAssetsFolder() { return std::filesystem::current_path().append("Assets"); }
+    static std::filesystem::path GetAssetAt(const std::string& file)
+    {
+        return GetAssetsFolder().append(file).make_preferred();
+    }
+};
